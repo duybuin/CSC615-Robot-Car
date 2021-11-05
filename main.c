e21b318c67540c15371be625712c50456eda0142
Kyle Gilbert <kyle.gilbert1622@gmail.com>
2021-11-04 17:11:06 -0700

    added program quit

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <signal.h>
+
 #include <wiringPi.h>
 #include <softPwm.h>
 
@@ -8,6 +11,15 @@
 #define MOTOR2 3
 #define MOTOR3 4
 #define MOTOR4 5
+
+void Handler(int signo) {
+  printf("Motor Stop\n");
+  softPwmWrite(MOTOR1, 0);
+  softPwmWrite(MOTOR2, 0);
+  softPwmWrite(MOTOR3, 0);
+  softPwmWrite(MOTOR4, 0);
+  exit(0);
+}
 
 int main() {
     wiringPiSetup();
@@ -21,9 +33,18 @@ int main() {
     softPwmCreate(MOTOR3, 0, 100);
     softPwmCreate(MOTOR4, 0, 100);
 
+    signal(SIGINT, Handler);
+
     softPwmWrite(MOTOR1, 100);
     softPwmWrite(MOTOR2, 100);
     softPwmWrite(MOTOR3, 100);
     softPwmWrite(MOTOR4, 100);
+
+    while(1) {
+
+    }
+
+    DEV_ModuleExit();
+
     return 0;
 }
